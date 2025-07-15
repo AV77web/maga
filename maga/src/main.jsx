326a214d76e741128e06d04311845b8eafe1cedf
AV77web <villari.andrea@libero.it>
2025-07-15 18:09:24 +0200

    modifica a main.jsx

--- maga/src/main.jsx
+++ maga/src/main.jsx
@@ -3,11 +3,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Crea una sola istanza di QueryClient per tutta l'app
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
